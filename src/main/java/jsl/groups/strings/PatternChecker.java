1d76b76e099ef3af0d601b00528aeae895b3aee4
Justice Sam <fjaccobian@gmail.com>
2024-06-25 23:05:13 -0400

    Initial commit

---
+++ src/main/java/jsl/groups/strings/PatternChecker.java
@@ -0,0 +1,47 @@
+package jsl.groups.strings;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * The type Pattern checker.
+ * @author github.com/Just-StyX
+ */
+public class PatternChecker {
+    public static void main(String[] args) {
+        System.out.println(patternChecker("abba", "foo baz buz foo"));
+    }
+
+    /**
+     * Pattern checker boolean.
+     * @author github.com/Just-StyX
+     *
+     * @param pattern the pattern
+     * @param input   the input
+     * @return the boolean
+     */
+    public static boolean patternChecker(String pattern, String input) {
+        final int lengthOfPattern = pattern.length();
+        final String[] strings = input.split(" ");
+        final int lengthOfStrings = strings.length;
+
+        if (lengthOfStrings != lengthOfPattern || (strings.length == 1 && strings[0].isEmpty())) {
+            return false;
+        }
+
+        final Map<Character, String> characterStringHashMap = new HashMap<>();
+
+        for (int i  = 0; i < lengthOfPattern; i++) {
+            char character = pattern.charAt(i);
+            String string = strings[i];
+
+            characterStringHashMap.putIfAbsent(character, string);
+
+            String assignedValue = characterStringHashMap.get(character);
+            if (!assignedValue.equals(string)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
